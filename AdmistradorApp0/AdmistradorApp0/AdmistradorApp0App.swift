5265c6618c8676d336c4e06c3761f3c44a4000f8
DTGocht <a01384738@itesm.mx>
2023-11-05 17:43:47 -0600

    Base
    Primer Commit

---
+++ AdmistradorApp0/AdmistradorApp0/AdmistradorApp0App.swift
@@ -0,0 +1,17 @@
+//
+//  AdmistradorApp0App.swift
+//  AdmistradorApp0
+//
+//  Created by Rebeca Cisneros Diaz on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AdmistradorApp0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
